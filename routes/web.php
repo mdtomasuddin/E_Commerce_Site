23481961bf2ecebd5200cb1e31881e154ac094e0
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-04-12 09:35:10 +0600

    welcome

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Frontend\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[WelcomeController::class,'index'])->name('welcome');
